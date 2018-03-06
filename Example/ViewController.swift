2e1465f7497dbb8b44a3d980508ad88ed1bcd267
Robin Malhotra <me@rmalhotra.com>
2018-03-06 15:29:19 +0530

    Dropping deployment target to 9.0

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -28,13 +28,13 @@ class ViewController: UIViewController {
 		node1.setImageVM(.url(URL(string: "https://lorempixel.com/200/200/")!), placeholderImage: #imageLiteral(resourceName: "Image"))
 		
 		node2.frame = CGRect(x: 100, y: 0, width: 100, height: 100)
-		node2.setImageURL(URL.init(string: "https://kayako-mobile-testing.kayako.com/avatar/get/22e238af-5bcd-5159-a4ac-e7f016821151?1508139874"))
+		node2.setImageVM(.url(URL.init(string: "https://kayako-mobile-testing.kayako.com/avatar/get/22e238af-5bcd-5159-a4ac-e7f016821151?1508139874")!))
 		
 		node3.frame = CGRect(x: 200, y: 0, width: 100, height: 100)
-		node3.setImageURL(URL.init(string: "https://kayako-mobile-testing.kayako.com/avatar/get/1e4dd52f-6383-5e07-aed0-864c06d6b232?1508135367"))
+		node3.setImageVM(.url(URL.init(string: "https://kayako-mobile-testing.kayako.com/avatar/get/1e4dd52f-6383-5e07-aed0-864c06d6b232?1508135367")!))
 
 		DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-			PINCache.shared().removeAllObjects()
+			PINCache.shared.removeAllObjects()
 		}
 	}
 
